c8b18229ecdf38b44ed5d40d96cb64b20af1d61b
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2024-08-27 17:02:53 +0300

    test / as GET

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 app.use(cookieParser());
 
-router.get("/", async (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).json({ message: "Welcome To Crypto Watcher Api" });
 });
 
